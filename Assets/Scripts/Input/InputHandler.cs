faab8d920613f4c2dbfbfab7803c969a934ab8be
Suhdo <dungvipnr@gmail.com>
2023-03-15 16:25:04 +0700

    fix: tile map

---
+++ Assets/Scripts/Input/InputHandler.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class InputHandler : MonoBehaviour
+{
+    public Vector2 RawMovementInput { get; private set; }
+
+    public void OnMoveInput(InputAction.CallbackContext context)
+    {
+        RawMovementInput = context.ReadValue<Vector2>();
+        Debug.Log(RawMovementInput);
+    }
+}

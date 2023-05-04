cc544c9a13129acf94b073b4d27915d94d7557a1
Suhdo <dungvipnr@gmail.com>
2023-05-04 21:49:21 +0700

    feat: sound manager, UI sound

--- Assets/Scripts/UI/Menus/SettingMenu.cs
+++ Assets/Scripts/UI/Menus/SettingMenu.cs
@@ -1,9 +1,19 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class SettingMenu : MonoBehaviour
 {
+    public Slider musicSlider, sfxSlider;
+
+    private void Start()
+    {
+        musicSlider.value = PlayerPrefs.GetFloat("musicVolume");
+        sfxSlider.value = PlayerPrefs.GetFloat("sfxVolume");
+    }
+
     public void OnClick_Back()
     {
         MenuManager.Instance.OpenMenu(Menu.MAIN_MENU, gameObject);
@@ -12,5 +22,15 @@ public class SettingMenu : MonoBehaviour
     public void SetActive()
     {
         gameObject.SetActive(false);
+    }
+
+    public void MusicVolume()
+    {
+        SoundManager.Instance.MusicVolume(musicSlider.value);
+    }
+
+    public void SFXVolume()
+    {
+        SoundManager.Instance.SFXVolume(sfxSlider.value);
     }
 }
